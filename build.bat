2a1a007b00e46c1c248d77cfe4a5d0e06b2d3646
leeboya <leegit@foxmail.com>
2018-09-06 00:02:05 +0800

    first commit

---
+++ build.bat
@@ -0,0 +1,4 @@
+@echo off
+    stylus -w styl/ -o css/
+    stylus -w styl/view/ -o css/view/
+exit
---
+++ js/home.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    window.mySwipe = new Swipe(document.getElementById('slider'), {
+        startSlide: 2,
+        speed: 400,
+        auto: 3000,
+        continuous: true,
+        disableScroll: false,
+        stopPropagation: false,
+        callback: function (index, elem) { },
+        transitionEnd: function (index, elem) { }
+    });
+}
